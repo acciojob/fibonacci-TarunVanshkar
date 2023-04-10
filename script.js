29101a3c300f0eb2ef1579dfbb895f8f4ba268e0
TarunVanshkar <117818351+TarunVanshkar@users.noreply.github.com>
2023-04-10 15:38:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
-// your code here
+	// your code here
+	let a=0;
+	let b=1;
+	if(num==0)
+		return a;
+
+	if(num==1)
+		return b;
+
+	let count=2;
+	let c=0;
+	while(count<=num)
+		{
+			c=a+b;
+			a=b;
+			b=c;
+		}
+	return c;
 }
 
 module.exports = fibonacci;
